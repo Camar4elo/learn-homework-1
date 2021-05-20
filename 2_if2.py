9559906d35537fbc853dccc4cb03c9d7e7f95847
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-05-20 11:19:30 +0300

    Changed if construction

--- 2_if2.py
+++ 2_if2.py
@@ -16,20 +16,21 @@
 """
 
 def main(string_1, string_2):
-    if type(string_1) == str and type(string_2) == str:
-        if string_1 == string_2:
-            return 1
-        elif string_1 != string_2 and string_2 == 'learn':
-            return 3
-        elif string_1 != string_2 and len(string_1) > len(string_2):
-            return 2
-    else:
+    if type(string_1) != str or type(string_2) != str:
         return 0
+    elif string_1 == string_2:
+        return 1
+    elif string_1 != string_2 and string_2 == 'learn':
+        return 3
+    elif string_1 != string_2 and len(string_1) > len(string_2):
+        return 2
     
 if __name__ == "__main__":
     count = 0
     while count <= 5 :
         string_1 = input('Введите строку 1: ')
+        print(type(string_1))
         string_2 = input('Введите строку 2: ')
+        print(type(string_2))
         print(main(string_1, string_2))
         count += 1
